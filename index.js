841c51c0ff379cda4c59210c1188725b8b36691b
Denzel Wamburu <denzjoseph@gmail.com>
2017-05-09 15:32:19 +0300

    use commander

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+'use strict';
 const chalk       = require('chalk');
 const clear       = require('clear');
 const CLI         = require('clui');
@@ -10,14 +12,24 @@ const touch       = require('touch');
 const fs          = require('fs');
 const files = require('./lib/files');
 const path = require('path');
+const program = require('commander');
+
 
 
 path.basename(path.dirname(fs.realpathSync(__filename)));
 path.basename(process.cwd());
 
-clear();
-console.log(
-  chalk.yellow(
-    figlet.textSync('MOS CLI', { horizontalLayout: 'full' })
-  )
-);
+// clear();
+// console.log(
+//   chalk.yellow(
+//     figlet.textSync('MOS CLI', { horizontalLayout: 'full' })
+//   )
+// );
+program
+  .version('0.0.1')
+  .command('')
+  .description('')
+  .option('','')
+  .option('','')
+  .action('');
+program.parse(process.argv);
