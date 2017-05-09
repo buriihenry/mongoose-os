9630b77aa12fb3c07bc0084e73ac8e5051a033c7
Denzel Wamburu <denzjoseph@gmail.com>
2017-05-09 15:43:16 +0300

    Add helper and Welcome Text

--- index.js
+++ index.js
@@ -13,23 +13,54 @@ const fs          = require('fs');
 const files = require('./lib/files');
 const path = require('path');
 const program = require('commander');
-
+const exec = require('child_process');
+const pkg = require('./package.json');
 
 
 path.basename(path.dirname(fs.realpathSync(__filename)));
 path.basename(process.cwd());
 
 // clear();
-// console.log(
-//   chalk.yellow(
-//     figlet.textSync('MOS CLI', { horizontalLayout: 'full' })
-//   )
-// );
+const welcometxt = console.log(
+  chalk.yellow(
+    figlet.textSync('MOS CLI', { horizontalLayout: 'full' })
+  )
+);
+/**
+ * list function definition
+ *
+ */
+let list = (directory,options)  => {
+    const cmd = 'ls';
+    let params = [];
+    
+    if (options.all) params.push("a");
+    if (options.long) params.push("l");
+    let parameterizedCommand = params.length 
+                                ? cmd + ' -' + params.join('') 
+                                : cmd ;
+    if (directory) parameterizedCommand += ' ' + directory ;
+    
+    let output = (error, stdout, stderr) => {
+        if (error) console.log(chalk.red.bold.underline("exec error:") + error);
+        if (stdout) console.log(chalk.green.bold.underline("Result:") + stdout);
+        if (stderr) console.log(chalk.red("Error: ") + stderr);
+    };
+    
+    exec(parameterizedCommand,output);
+    
+};
+
+
 program
-  .version('0.0.1')
-  .command('')
-  .description('')
-  .option('','')
-  .option('','')
-  .action('');
+    .version(pkg.version)
+    .command('list [directory]')
+    .option('-a, --all', 'List all')
+    .option('-l, --long','Long list format')
+    // .action(list);
+    .action(welcometxt);
+
 program.parse(process.argv);
+
+// if program was called with no arguments, show help.
+if (program.args.length === 0) program.help();
